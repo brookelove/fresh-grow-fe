b46a034f328cbd872427f12e44ea90536164c5d5
brookelove <sblove@bu.edu>
2023-09-15 11:03:18 -0400

    adding search functionality to the code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,16 @@ export default function Header({showCart}){
     const [isSearching, setSearching] = useState(false);
     const [user, setUserName] = useState(null);
     const [logout, setLogout] = useState(false);
-    const categoryCardRef = useRef()
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [shouldShowResults, setShouldShowResults] = useState(false);
 
-    let handleSubmit = async (event) => {
+
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+      };
+
+    let handleLogoutSubmit = async (event) => {
 
         const options = {
             method: 'POST',
@@ -41,11 +48,42 @@ export default function Header({showCart}){
             console.error("Error ", error);
         }
     };
+    // take to refactor in a section called api to make all api calls from
+        //call to the products first
+        let fetchProducts = async () => {
+            const response = await fetch('http://localhost:3001/api/products');
+            const data = response.json()
+            return data
+        }
+        
+        // then filter the produts and then set filted products to the filtered information 
+        const filterProducts = (products) => {
+            if(products && products.filter){
+            const filtered = products.filter((product) =>
+              product.product_name.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setFilteredProducts(filtered);
+            }
+          };
+
+          // to allow the search to happen on the search button
+    const search = async () => {
+        const data = await fetchProducts();
+        filterProducts(data);
+        setShouldShowResults(true);
+    }
 
 
     useEffect(()=> {
     let username = localStorage.getItem("username")
-    console.log(username)
+    
+    // call to get data then filter that data had to wrap to account for the async of the functions
+    const fetchDataAndFilter = async () => {
+        const data = await fetchProducts();
+        filterProducts(data);
+      };
+
+
     if(username != null || username != undefined) {
         setUserName(username)
         setLogout(true)
@@ -65,23 +103,23 @@ export default function Header({showCart}){
             }
         }
     }
-
     window.addEventListener('scroll', stickyNavbar)
     
     // Call initially to set the position 
     stickyNavbar();
-
+    fetchDataAndFilter();
     return ()=> {
         window.removeEventListener('scroll', stickyNavbar)
     }
     }, [])
+
+
     let openSearch = () =>{
         setSearching(!isSearching);
     } 
 
     const hoverTitle = () => {
         // Update the title text on hover
-        console.log("hover")
         setTitleText('GLOW BRIGHT');
     };
 
@@ -90,6 +128,7 @@ export default function Header({showCart}){
         console.log("over");
         setTitleText('FRESH GLOW');
     };
+    
 
     return (
         <header id='navbar' ref={navEl} >
@@ -110,7 +149,7 @@ export default function Header({showCart}){
                         <a onClick={openSearch}>SEARCH</a>
                     )}
                     {logout ? (
-                        <a>LOGOUT</a>
+                        <a onClick={handleLogoutSubmit}>LOGOUT</a>
                         
                     ) : (
                         <Link to="/login">LOGIN</Link>
@@ -124,9 +163,17 @@ export default function Header({showCart}){
              {/* change from hide to show in the code */}
             {isSearching && (
                 <div className='searchContainer'>
-                <input text="search"></input>
-                <a>SEARCH</a>
+                <input text="search" placeholder="search for items" value={searchQuery} onChange={handleSearchChange}></input>
+                <a onClick={search}>SEARCH</a>
             </div>
+            )}
+            {/* Display search results */}
+            {isSearching && shouldShowResults && (
+                <div>
+                  {filteredProducts.map(product => (
+                    <li key={product.id}>{product.product_name}</li>
+                  ))}
+                </div>
             )}
             
         </header>
